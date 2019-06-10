b95e881b6bddf5cc218caaed83c3670940a5ad4f
itning <itning@itning.top>
2019-06-10 09:08:30 +0800

    init all

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,100 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <a-layout style="height: 100%">
+    <a-layout-sider
+      :trigger="null"
+      collapsible
+      v-model="collapsed"
+    >
+      <div class="logo-box">
+        <img class="logo" :src="`${publicPath}logo.png`" alt="logo img"/>
+      </div>
+      <a-menu theme="dark" mode="inline" :defaultSelectedKeys="['3']">
+        <a-menu-item key="1">
+          <a-icon type="plus"/>
+          <span>新增计划</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="profile"/>
+          <span>我的审核</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <a-icon type="align-left"/>
+          <span>计划列表</span>
+        </a-menu-item>
+        <a-menu-item key="4">
+          <a-icon type="download"/>
+          <span>导出表格</span>
+        </a-menu-item>
+        <a-menu-item key="5">
+          <a-icon type="edit"/>
+          <span>折扣管理</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <a-icon
+          class="trigger"
+          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+          @click="()=> collapsed = !collapsed"
+        />
+        <a-dropdown class="user-btn">
+          <a class="ant-dropdown-link">
+            {{loginUser}}
+            <a-icon type="down"/>
+          </a>
+          <a-menu slot="overlay">
+            <a-menu-item>
+              <a href="javascript:;">注销登录</a>
+            </a-menu-item>
+          </a-menu>
+        </a-dropdown>
+      </a-layout-header>
+      <a-layout-content :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }">
+        Content
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
 
+<script>
+  export default {
+    name: 'App',
+    data: () => ({
+      publicPath: process.env.BASE_URL,
+      //关闭菜单栏
+      collapsed: false,
+      //登录用户
+      loginUser: "教务处：舒露"
+    }),
+  }
+</script>
+
+<style scoped>
+  /*调整菜单栏开关按钮位置*/
+  .trigger {
+    padding: 20px;
+  }
+
+  /*logo盒子*/
+  .logo-box {
+    padding: 0 10px;
+    line-height: 64px;
+    height: 64px;
+    overflow: hidden;
+  }
+
+  .logo-box img {
+    width: 180px;
+  }
+
+  /*用户操作按钮*/
+  .user-btn {
+    padding-right: 24px;
+    float: right;
+  }
+</style>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
