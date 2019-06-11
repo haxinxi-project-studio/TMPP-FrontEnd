0c76fb2e827188d4e951c61ad4d3d2c4bfc5247d
itning <itning@itning.top>
2019-06-11 09:32:09 +0800

    init plan list

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,84 @@
 <template>
-  <a-layout style="height: 100%">
-    <a-layout-sider
-      :trigger="null"
-      collapsible
-      v-model="collapsed"
-    >
-      <div class="logo-box">
-        <img class="logo" :src="`${publicPath}logo.png`" alt="logo img"/>
-      </div>
-      <a-menu theme="dark" mode="inline" :defaultSelectedKeys="selectedMenuKeys">
-        <a-menu-item key="new_plan" @click="pushRouter('/new_plan')">
-          <a-icon type="plus"/>
-          <span>新增计划</span>
-        </a-menu-item>
-        <!--START：只有教务处与办公室主任才显示-->
-        <a-menu-item key="review" @click="pushRouter('/review')">
-          <a-icon type="profile"/>
-          <span>我的审核</span>
-        </a-menu-item>
-        <!--END：只有教务处与办公室主任才显示-->
-        <a-menu-item key="plan_list" @click="pushRouter('/plan_list')">
-          <a-icon type="align-left"/>
-          <span>计划列表</span>
-        </a-menu-item>
-        <a-menu-item key="export" @click="pushRouter('/export')">
-          <a-icon type="download"/>
-          <span>导出表格</span>
-        </a-menu-item>
-        <a-menu-item key="discount" @click="pushRouter('/discount')">
-          <a-icon type="edit"/>
-          <span>折扣管理</span>
-        </a-menu-item>
-        <!--START：只有教师角色才显示-->
-        <a-menu-item key="buy" @click="pushRouter('/buy')">
-          <a-icon type="profile"/>
-          <span>我的购书</span>
-        </a-menu-item>
-        <a-menu-item key="add" @click="pushRouter('/add')">
-          <a-icon type="edit"/>
-          <span>填写购书信息</span>
-        </a-menu-item>
-        <!--END：只有教师角色才显示-->
-      </a-menu>
-    </a-layout-sider>
-    <a-layout>
-      <a-layout-header style="background: #fff; padding: 0">
-        <a-icon
-          class="trigger"
-          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
-          @click="()=> collapsed = !collapsed"
-        />
-        <a-dropdown class="user-btn">
-          <a class="ant-dropdown-link">
-            {{loginUser}}
-            <a-icon type="down"/>
-          </a>
-          <a-menu slot="overlay">
-            <a-menu-item>
-              <a href="javascript:;">注销登录</a>
-            </a-menu-item>
-          </a-menu>
-        </a-dropdown>
-      </a-layout-header>
-      <a-layout-content>
-        <router-view/>
-      </a-layout-content>
+  <a-locale-provider :locale="locale">
+    <a-layout style="height: 100%">
+      <a-layout-sider
+        :trigger="null"
+        collapsible
+        v-model="collapsed"
+      >
+        <div class="logo-box">
+          <img class="logo" :src="`${publicPath}logo.png`" alt="logo img"/>
+        </div>
+        <a-menu theme="dark" mode="inline" :defaultSelectedKeys="selectedMenuKeys">
+          <a-menu-item key="new_plan" @click="pushRouter('/new_plan')">
+            <a-icon type="plus"/>
+            <span>新增计划</span>
+          </a-menu-item>
+          <!--START：只有教务处与办公室主任才显示-->
+          <a-menu-item key="review" @click="pushRouter('/review')">
+            <a-icon type="profile"/>
+            <span>我的审核</span>
+          </a-menu-item>
+          <!--END：只有教务处与办公室主任才显示-->
+          <a-menu-item key="plan_list" @click="pushRouter('/plan_list')">
+            <a-icon type="align-left"/>
+            <span>计划列表</span>
+          </a-menu-item>
+          <a-menu-item key="export" @click="pushRouter('/export')">
+            <a-icon type="download"/>
+            <span>导出表格</span>
+          </a-menu-item>
+          <a-menu-item key="discount" @click="pushRouter('/discount')">
+            <a-icon type="edit"/>
+            <span>折扣管理</span>
+          </a-menu-item>
+          <!--START：只有教师角色才显示-->
+          <a-menu-item key="buy" @click="pushRouter('/buy')">
+            <a-icon type="profile"/>
+            <span>我的购书</span>
+          </a-menu-item>
+          <a-menu-item key="add" @click="pushRouter('/add')">
+            <a-icon type="edit"/>
+            <span>填写购书信息</span>
+          </a-menu-item>
+          <!--END：只有教师角色才显示-->
+        </a-menu>
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header style="background: #fff; padding: 0">
+          <a-icon
+            class="trigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="()=> collapsed = !collapsed"
+          />
+          <a-dropdown class="user-btn">
+            <a class="ant-dropdown-link">
+              {{loginUser}}
+              <a-icon type="down"/>
+            </a>
+            <a-menu slot="overlay">
+              <a-menu-item>
+                <a href="javascript:;">注销登录</a>
+              </a-menu-item>
+            </a-menu>
+          </a-dropdown>
+        </a-layout-header>
+        <a-layout-content>
+          <router-view/>
+        </a-layout-content>
+      </a-layout>
     </a-layout>
-  </a-layout>
+  </a-locale-provider>
 </template>
 
 <script>
+  import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN';
+
   export default {
     name: 'App',
     data: () => ({
       publicPath: process.env.BASE_URL,
+      locale: zhCN,
       //关闭菜单栏
       collapsed: false,
       //登录用户
--- src/views/aao/PlanList.vue
+++ src/views/aao/PlanList.vue
@@ -2,12 +2,70 @@
   <div>
     <content-title :title="titleInfo.title" :subtitle="titleInfo.subtitle"/>
     <div class="content-style">
+      <a-radio-group>
+        <a-radio-button value="all">全部</a-radio-button>
+        <a-radio-button value="processing">进行中</a-radio-button>
+        <a-radio-button value="done">已完成</a-radio-button>
+      </a-radio-group>
+
+      <a-table :columns="columns"
+               :rowKey="record => record.id"
+               :dataSource="data"
+               :pagination="pagination"
+               :loading="loading"
+               @change="handleTableChange"
+               :scroll="{ x: 1000 }"
+               class="table-box"
+      >
+        <template slot="action" slot-scope="text">
+          <a href="">删除</a>&nbsp;
+          <a href="">下载执行计划</a>
+        </template>
+      </a-table>
     </div>
   </div>
 </template>
 
 <script>
   import ContentTitle from "@/components/ContentTitle";
+  //表头
+  const columns = [
+    {
+      title: '学年',
+      dataIndex: 'year'
+    },
+    {
+      title: '学期',
+      dataIndex: 'term'
+    },
+    {
+      title: '授课部门',
+      dataIndex: 'teachingDepartment'
+    },
+    {
+      title: '教育层次',
+      dataIndex: 'educationalLevel'
+    },
+    {
+      title: '计划创建时间',
+      dataIndex: 'startTime'
+    },
+    {
+      title: '计划完成时间',
+      dataIndex: 'endTime'
+    },
+    {
+      title: '状态',
+      dataIndex: 'approvalStatus'
+    },
+    {
+      title: '操作',
+      key: 'operation',
+      fixed: 'right',
+      width: 155,
+      scopedSlots: {customRender: 'action'}
+    },
+  ];
 
   export default {
     name: "PlanList",
@@ -18,12 +76,106 @@
           title: '计划列表',
           subtitle: '展示历史完成的执行计划和目前正在进的计划，并展示计划状态',
         },
+        data: [],
+        pagination: {
+          //是否可以快速跳转至某页
+          showQuickJumper: true,
+          //是否可以改变 pageSize
+          showSizeChanger: true,
+          //默认的每页条数
+          defaultPageSize: 50
+        },
+        loading: false,
+        columns,
       };
     },
-    methods: {},
+    methods: {
+      handleTableChange(pagination, filters, sorter) {
+        console.log(pagination);
+        const pager = {...this.pagination};
+        pager.current = pagination.current;
+        this.pagination = pager;
+        this.fetch({
+          results: pagination.pageSize,
+          page: pagination.current,
+          sortField: sorter.field,
+          sortOrder: sorter.order,
+          ...filters,
+        });
+      },
+      fetch(params = {}) {
+        console.log('params:', params);
+        this.loading = true;
+
+        const json = {
+          "code": 200,
+          "msg": "获取计划列表成功",
+          "data": {
+            "pageNum": 1,
+            "pageSize": 2,
+            "size": 5,
+            "orderBy": null,
+            "startRow": 1,
+            "endRow": 5,
+            "total": 11,
+            "pages": 3,
+            "list": [{
+              "id": "1",
+              "year": "2018-2019",
+              "term": 1,
+              "teachingDepartment": "软件学院",
+              "educationalLevel": "本科",
+              "startTime": "2017-10-01 12：00",
+              "endTime": "2017-10-01 12：00",
+              "approvalStatus": "进行中"
+            },
+              {
+                "id": "2",
+                "year": "2018-2019",
+                "term": 1,
+                "teachingDepartment": "软件学院",
+                "educationalLevel": "本科",
+                "startTime": "2017-10-01 12：00",
+                "endTime": "2017-10-01 12：00",
+                "approvalStatus": "已完成"
+              }
+            ],
+            "prePage": 0,
+            "nextPage": 2,
+            "isFirstPage": true,
+            "isLastPage": false,
+            "hasPreviousPage": false,
+            "hasNextPage": true,
+            "navigatePages": 8,
+            "navigatepageNums": [
+              1,
+              2,
+              3
+            ],
+            "navigateFirstPage": 1,
+            "navigateLastPage": 3,
+            "firstPage": 1,
+            "lastPage": 3
+          }
+        };
+
+        const pagination = {...this.pagination};
+        // Read total count from server
+        // pagination.total = data.totalCount;
+        pagination.total = json.data.total;
+        this.loading = false;
+        this.data = json.data.list;
+        this.pagination = pagination;
+      }
+    },
+    mounted() {
+      this.fetch();
+    },
   }
 </script>
 
 <style scoped>
-
+  .table-box {
+    margin-top: 20px;
+  }
 </style>
